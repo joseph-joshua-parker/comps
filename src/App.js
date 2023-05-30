4bb89e4e3f6b56de6a729be7c3b4b1d2ddeb94cf
joseph-joshua-parker <joelnashjobs@yahoo.com>
2023-05-30 08:14:51 -0300

    Finished Section 10: Custom Navigation & Routing (But really buttons)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import {GoBell, GoCloudDownload, GoDatabase} from 'react-icons/go';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const handleClick = ()=>{
+	console.log('fgjnf')
+}
+
+const App = ()=>{
+	return (
+		<div>
+			<div>
+				<Button primary onClick={handleClick}>
+					<GoBell/>
+					Ring ring
+				</Button>
+			</div>
+			<div>
+				<Button success rounded>
+					<GoCloudDownload/>
+					Upload
+				</Button>
+			</div>
+			<div>
+				<Button danger primary>
+					<GoDatabase/>
+					Save
+				</Button>
+			</div>
+			<div>
+				<Button></Button>
+			</div>
+		</div>
+	)
 }
 
 export default App;
